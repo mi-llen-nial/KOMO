191ab0a4b635d962bcafe177ae5204d7b4f78863
mi_llen_nial <aktanartyshovmmm@gmail.com>
2025-09-29 20:55:17 +0500

    change JS request

--- website/frontend/JavaScript/main.js
+++ website/frontend/JavaScript/main.js
@@ -1,12 +1,21 @@
 const buttonSignIn = document.getElementById("buttonSignIn");
 
 buttonSignIn.addEventListener("click", () => {
-    fetch("http://127.0.0.1:8000/authorization")
-        .then(res => res.json())
-        .then(data => {
-            console.log('Users: ', data);
-        } )
-        .catch(error => {
-            console.log('Error: ', error);
-        })
-})
+    try {
+        const response = await fetch("http://127.0.0.1:8000/authorization", {
+            method: "POST", 
+            headers: {
+                "Content-type": "applicatoin/json"
+            }, 
+            body: JSON.stringify({
+                user_name: "user_name",
+                user_password: "user_password"
+            })
+        });
+        const data = await response.json();
+        console.log("Users", data);
+        catch(error) {
+            console.log("Error", error);
+        }
+    }   
+});
