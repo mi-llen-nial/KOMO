bc3f6d84ba96672d1f39c43d699357fe16df58b1
mi_llen_nial <aktanartyshovmmm@gmail.com>
2025-09-30 16:42:13 +0500

    create the request in JS

--- website/frontend/JavaScript/main.js
+++ website/frontend/JavaScript/main.js
@@ -1,10 +1,10 @@
 const buttonSignIn = document.getElementById("buttonSignIn");
 
 buttonSignIn.addEventListener("click", () => {
-    const userName = document.getElementById("user_name").value;
-    const userPassword = document.getElementById("user_password").value;
+    const userNameIn = document.getElementById("user_name_in").value;
+    const userPasswordIn = document.getElementById("user_password_in").value;
 
-    async function userData() {
+    async function userDataIn() {
         try {
             const response = await fetch("http://127.0.0.1:8000/authorization", {
                 method: "POST", 
@@ -12,8 +12,8 @@ buttonSignIn.addEventListener("click", () => {
                     "Content-type": "application/json"
                 }, 
                 body: JSON.stringify({
-                    'userName': userName,
-                    'userPassword': userPassword
+                    'userNameIn': userNameIn,
+                    'userPasswordIn': userPasswordIn
                 })
             });
             const data = await response.json();
@@ -23,4 +23,36 @@ buttonSignIn.addEventListener("click", () => {
             console.log("Error", error);
         }
     }
+    userDataIn();
+});
+
+
+const buttonSignUp = document.getElementById("buttonSignUp");
+
+buttonSignUp.addEventListener("click", () => {
+    const userNameUp = document.getElementById("user_name_up").value;
+    const userEmailUp = document.getElementById("user_email_up").value;
+    const userPasswordUp = document.getElementById("user_password_up").value;
+
+    async function userDataUp() {
+        try {
+            const response = await fetch("http://127.0.0.1:8000/authorization", {
+                method: "POST", 
+                headers: {
+                    "Content-type": "application/json"
+                }, 
+                body: JSON.stringify({
+                    'userNameUp': userNameUp,
+                    'userEmailUp': userEmailUp,
+                    'userPasswordUp': userPasswordUp
+                })
+            });
+            const data = await response.json();
+            console.log("Users", data);
+        } 
+        catch(error) {
+            console.log("Error", error);
+        }
+    }
+    userDataUp();
 });
